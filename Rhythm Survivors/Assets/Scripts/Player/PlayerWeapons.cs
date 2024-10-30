e3fab6227f55a1fa73cb803d2b56718652e42f3a
bbailey0628 <bbailey0628@hotmail.com>
2024-10-30 13:40:25 -0400

    Weapon list change
    The way the game tracks what weapons you have was revamped

--- Rhythm Survivors/Assets/Scripts/Player/PlayerWeapons.cs
+++ Rhythm Survivors/Assets/Scripts/Player/PlayerWeapons.cs
@@ -9,8 +9,14 @@ public class PlayerWeapons : MonoBehaviour
     public GameObject bow, staff, sword;
     public Sprite bowSprite, staffSprite, swordSprite;
 
-    private List<string> weapons = new List<string>(); // List of weapons
-    private int num = 0;
+    private List<(string, int)> items = new List<(string, int)>()
+    { 
+        ("Bow", 0), ("Staff", 0), ("Sword", 0)
+    };
+    public int maxWeapons;
+    public int maxTrinkets;
+    private int numWeapons;
+    private int numTrinkets;
 
     // Start is called before the first frame update
     void Start()
@@ -24,42 +30,74 @@ public class PlayerWeapons : MonoBehaviour
         // Temporary testing inputs
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            if (!weapons.Contains("Bow"))
+            if (items[FindItem("Bow")].Item2 == 0)
             {
-                weapons.Add("Bow");
-                Instantiate(bow, this.transform);
-                AddUIElement(bowSprite, bow.GetComponent<Weapon>().requiredBeats);
-                num++;
+                ProgressItem("Bow");
             }
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            if (!weapons.Contains("Staff"))
+            if (items[FindItem("Staff")].Item2 == 0)
             {
-                weapons.Add("Staff");
-                Instantiate(staff, this.transform);
-                AddUIElement(staffSprite, staff.GetComponent<Weapon>().requiredBeats);
-                num++;
+                ProgressItem("Staff");
             }
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            if (!weapons.Contains("Sword"))
+            if (items[FindItem("Sword")].Item2 == 0)
             {
-                weapons.Add("Sword");
-                Instantiate(sword, this.transform);
-                AddUIElement(swordSprite, sword.GetComponent<Weapon>().requiredBeats);
-                num++;
+                ProgressItem("Sword");
             }
         }
+    }
+
+    private int FindItem(string name)
+    {
+        for (int i = 0; i < items.Count; i++)
+        {
+            if (items[i].Item1 == name)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void ProgressItem(string name)
+    {
+        int index = FindItem(name);
+
+        switch (name)
+        {
+            case "Bow":
+                Instantiate(bow, this.transform);
+                AddUIElement(bowSprite, bow.GetComponent<Weapon>().requiredBeats);
+                numWeapons++;
+                break;
+            case "Staff":
+                Instantiate(staff, this.transform);
+                AddUIElement(staffSprite, staff.GetComponent<Weapon>().requiredBeats);
+                numWeapons++;
+                break;
+            case "Sword":
+                Instantiate(sword, this.transform);
+                AddUIElement(swordSprite, sword.GetComponent<Weapon>().requiredBeats);
+                numWeapons++;
+                break;
+            default:
+                Debug.LogWarning(name + " not found in item list.");
+                break;
+        }
+
+        items[index] = (name, items[index].Item2 + 1);
     }
 
     private void AddUIElement(Sprite weaponSprite, int beats)
     {
         GameObject newUI = Instantiate(weaponUI, weaponDisplay.transform);
-        newUI.GetComponent<RectTransform>().anchoredPosition += Vector2.left * (newUI.GetComponent<RectTransform>().rect.width * num);
+        newUI.GetComponent<RectTransform>().anchoredPosition += Vector2.left * (newUI.GetComponent<RectTransform>().rect.width * numWeapons);
         newUI.GetComponentInChildren<WeaponBeatIndicatorManager>().requiredBeats = beats;
         newUI = newUI.transform.GetChild(0).gameObject;
         newUI.GetComponent<Image>().sprite = weaponSprite;
