e42a80c2b5402cb4a174955f85d79926305b0e8f
bbailey0628 <bbailey0628@hotmail.com>
2024-11-04 11:02:59 -0500

    Enemy Arrow fix
    Enemy Arrow was using wrong collider function
    Player collider reenabled (I don't know why it was disabled)
    Enemy Arrow works in build, base enemy collisions don't

--- Rhythm Survivors/Assets/Scripts/Enemies/EnemyArrow.cs
+++ Rhythm Survivors/Assets/Scripts/Enemies/EnemyArrow.cs
@@ -32,7 +32,7 @@ public class EnemyArrow : MonoBehaviour
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
